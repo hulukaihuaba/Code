c69adec8177843aa9c283b3128019a4979fdb6ce
hulukaihuaba <15700086047@163.com>
2015-05-13 14:04:24 +0800

    工具类

---
+++ MusicPlayer/src/com/sgf/helper/MediaUtil.java
@@ -0,0 +1,65 @@
+package com.sgf.helper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.provider.MediaStore;
+
+import com.sgf.model.Music;
+
+public class MediaUtil {
+
+	public static List<Music> getMusicList(Context context) {
+		Cursor cursor = context.getContentResolver().query(
+				MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, null, null, null,
+				MediaStore.Audio.Media.DEFAULT_SORT_ORDER);
+		List<Music> musicList = new ArrayList<Music>();
+		while (cursor.moveToNext()) {
+			Music music = new Music();
+			String title = cursor.getString(cursor
+					.getColumnIndex(MediaStore.Audio.Media.TITLE));
+			String artist = cursor.getString(cursor
+					.getColumnIndex(MediaStore.Audio.Media.ARTIST));
+			// String album = cursor.getString(cursor
+			// .getColumnIndex(MediaStore.Audio.Media.ALBUM));
+			long size = cursor.getLong(cursor
+					.getColumnIndex(MediaStore.Audio.Media.SIZE));
+			long duration = cursor.getLong(cursor
+					.getColumnIndex(MediaStore.Audio.Media.DURATION));
+			String url = cursor.getString(cursor
+					.getColumnIndex(MediaStore.Audio.Media.DATA));
+			int id = cursor.getInt(cursor
+					.getColumnIndex(MediaStore.Audio.Media._ID));
+			music.setId(id);
+			music.setUrl(url);
+			music.setSize(size);
+			music.setArtist(artist);
+			music.setTitle(title);
+			music.setDuration(duration);
+			musicList.add(music);
+		}
+		return musicList;
+	}
+
+	public static String formatTime(long time) {
+		String min = time / (1000 * 60) + "";
+		String sec = time % (1000 * 60) + "";
+		if (min.length() < 2) {
+			min = "0" + time / (1000 * 60) + "";
+		} else {
+			min = time / (1000 * 60) + "";
+		}
+		if (sec.length() == 4) {
+			sec = "0" + (time % (1000 * 60)) + "";
+		} else if (sec.length() == 3) {
+			sec = "00" + (time % (1000 * 60)) + "";
+		} else if (sec.length() == 2) {
+			sec = "000" + (time % (1000 * 60)) + "";
+		} else if (sec.length() == 1) {
+			sec = "0000" + (time % (1000 * 60)) + "";
+		}
+		return min + ":" + sec.trim().substring(0, 2);
+	}
+}
